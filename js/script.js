4a691e5a360bd33f7a6d2731a4ea14a0c40296a4
Jon <jhjsimpson@gmail.com>
2022-01-13 17:22:20 +0000

    JS working edit and delete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const albumCollection = [];
+let currentrowID = 0;
 
 function addAlbum() {
   const row = document.createElement("tr");
@@ -8,17 +9,17 @@ function addAlbum() {
   const editBtn = document.createElement("button");
   const deleteBtn = document.createElement("button");
 
-  editBtn.appendChild(document.createTextNode("Edit"));
+  editBtn.appendChild(document.createTextNode("Save"));
   deleteBtn.appendChild(document.createTextNode("Delete"));
 
   const artistOb = document.getElementById("addArtist").value;
   const albumOb = document.getElementById("addAlbum").value;
   const yearOb = document.getElementById("addYear").value;
 
-  let rowId = generateRowId();
+  let rowID = generaterowID();
 
   let albumInfo = {
-    ID: rowId,
+    ID: rowID,
     artist: artistOb,
     album: albumOb,
     year: yearOb,
@@ -34,9 +35,9 @@ function addAlbum() {
   row.appendChild(editBtn);
   row.appendChild(deleteBtn);
 
-  row.setAttribute("id", rowId);
-  editBtn.setAttribute("id", rowId);
-  deleteBtn.setAttribute("id", rowId);
+  row.setAttribute("id", rowID);
+  editBtn.setAttribute("id", rowID);
+  deleteBtn.setAttribute("id", rowID);
 
   editBtn.setAttribute("onclick", "editAlbum(this.id)");
   deleteBtn.setAttribute("onclick", "deleteAlbum(this.id)");
@@ -47,26 +48,70 @@ function addAlbum() {
   addToAlbums(albumInfo);
 }
 
-function deleteAlbum(rowId) {
-  let item = albumCollection.find((x) => x.ID == rowId);
+function deleteAlbum(rowID) {
+  let item = albumCollection.find((x) => x.ID == rowID);
   let index = albumCollection.indexOf(item);
+
   document.getElementById("table-albums").deleteRow(index + 1);
+
   albumCollection.splice(index, 1);
 }
 
-function editAlbum(rowId) {
-  let item = albumCollection.find((x) => x.ID == rowId);
-  console.log(item);
+function editAlbum(rowID) {
+  let item = albumCollection.find((x) => x.ID == rowID);
+  let index = albumCollection.indexOf(item);
+  item.artist = document.getElementById("addArtist").value;
+  item.album = document.getElementById("addAlbum").value;
+  item.year = document.getElementById("addYear").value;
+
+  albumCollection[index] = item;
+
+  const rowCount = document.getElementById("table-albums").rows.length;
+  for (i = rowCount; i > 1; i--) {
+    document.getElementById("table-albums").deleteRow(1);
+  }
+
+  albumCollection.forEach((object) => {
+    const row = document.createElement("tr");
+    const artist = document.createElement("td");
+    const album = document.createElement("td");
+    const year = document.createElement("td");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    editBtn.appendChild(document.createTextNode("Save"));
+    deleteBtn.appendChild(document.createTextNode("Delete"));
+
+    let rowID = generaterowID();
+
+    object.ID = rowID;
+    artist.innerHTML = object.artist;
+    album.innerHTML = object.album;
+    year.innerHTML = object.year;
+
+    row.appendChild(artist);
+    row.appendChild(album);
+    row.appendChild(year);
+    row.appendChild(editBtn);
+    row.appendChild(deleteBtn);
+
+    row.setAttribute("id", rowID);
+    editBtn.setAttribute("id", rowID);
+    deleteBtn.setAttribute("id", rowID);
+
+    editBtn.setAttribute("onclick", "editAlbum(this.id)");
+    deleteBtn.setAttribute("onclick", "deleteAlbum(this.id)");
+
+    let albumTable = document.getElementById("table-albums");
+    albumTable.appendChild(row);
+  });
 }
 
 function addToAlbums(albumInfo) {
   albumCollection.push(albumInfo);
 }
 
-let currentRowId = 0;
-function generateRowId() {
-  currentRowId++;
-  return currentRowId;
+function generaterowID() {
+  currentrowID++;
+  return currentrowID;
 }
-
-function returnAlbumIndex() {}
